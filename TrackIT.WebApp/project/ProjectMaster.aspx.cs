2a2ed8ea81592d9abc5188441e7dc898ed2e1210
varada-fintek <varadarajan.seshadri@stradegi.com>
2016-02-16 10:27:04 +0530

    project and its resource files modified

--- TrackIT.WebApp/project/ProjectMaster.aspx.cs
+++ TrackIT.WebApp/project/ProjectMaster.aspx.cs
@@ -26,9 +26,38 @@ namespace TrackIT.WebApp.project
 {
     public partial class ProjectMaster : BasePage
     {
+        #region Declarations
+        DBHelper.DBConnect ldbh_QueryExecutors = new DBHelper.DBConnect();
+        WebDataGrid iwdg_projectMasterGrid;
+        #endregion
         protected void Page_Load(object sender, EventArgs e)
         {
             lblCreateProjects.Text = RollupText("Projects", "lblCreateProjects");
+            iwdg_projectMasterGrid = new WebDataGrid();
+            pnl_projectGrid.Controls.Add(iwdg_projectMasterGrid);
+            TrackIT.WebApp.CommonSettings.ApplyGridSettings(iwdg_projectMasterGrid);
+
+            iwdg_projectMasterGrid.InitializeRow += iwdg_projectMasterGrid_InitializeRow;
+            DataSet lds_Result;
+            lds_Result = ldbh_QueryExecutors.ExecuteDataSet("select project_code,project_name,project_kickoff_date,project_owner,is_active from prj_projects");
+            if (lds_Result.Tables[0].Rows.Count > 0)
+            {
+                iwdg_projectMasterGrid.DataSource = lds_Result.Tables[0];
+                iwdg_projectMasterGrid.DataBind();
+            }
+        }
+        private void iwdg_projectMasterGrid_InitializeRow(object sender, RowEventArgs e)
+        {
+            //throw new NotImplementedException();
+            if (e.Row.Index == 0)
+            {
+
+                e.Row.Items.FindItemByKey("project_code").Column.Header.Text = RollupText("projects", "gridprojectcode");
+                e.Row.Items.FindItemByKey("project_name").Column.Header.Text = RollupText("projects", "gridprojectname");
+                e.Row.Items.FindItemByKey("project_kickoff_date").Column.Header.Text = RollupText("projects", "gridprojectkickoffdate");
+                e.Row.Items.FindItemByKey("project_owner").Column.Header.Text = RollupText("projects", "gridprojectowner");
+                e.Row.Items.FindItemByKey("is_active").Column.Header.Text = RollupText("projects", "gridisactive");
+            }
         }
     }
 }
